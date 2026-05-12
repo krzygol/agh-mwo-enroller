ed624cfff45cec4a893f8db553697ecc4d289413
Krzysztof Golebiowski <krzygol@gmail.com>
2026-05-12 23:53:38 +0200

    Authentication

--- src/main/java/security/WebSecurity.java
+++ src/main/java/security/WebSecurity.java
@@ -1,18 +1,52 @@
 package com.company.enroller.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    com.company.enroller.security.ParticipantProvider participantProvider;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Value("${security.secret}")
+    String secret;
+
+    @Value("${security.issuer}")
+    String issuer;
+
+    @Value("${security.tokenExpiration}")
+    int tokenExpiration;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest().permitAll()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(
+                        new com.company.enroller.security.JWTAuthenticationFilter(
+                                authenticationManager(),
+                                secret,
+                                issuer,
+                                tokenExpiration),
+                        UsernamePasswordAuthenticationFilter.class);;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
     }
 }
